76bf0a986ecfbd259f1bcaca3bed0c7134fb9533
vardeath <vardeath@yandex.ru>
2020-08-30 13:57:11 +0300

    api-iter2-v3

--- api/views.py
+++ api/views.py
@@ -7,6 +7,8 @@ from posts.models import Post, Comment
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.method == 'GET':
+            return True
         return obj.author == request.user
 
 
