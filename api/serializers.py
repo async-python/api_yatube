6eb4608fe76d708fe762011408a8a53ab2bd319a
vardeath <vardeath@yandex.ru>
2020-08-31 15:09:18 +0300

    api-iter3-v1

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,8 @@ from posts.models import Post, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
 
     class Meta:
         fields = '__all__'
@@ -11,7 +12,8 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
 
     class Meta:
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,21 +1,15 @@
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from api.serializers import PostSerializer, CommentSerializer
+from api.permissions import IsOwnerOrReadOnly
 from posts.models import Post, Comment
-
-
-class IsOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method == 'GET':
-            return True
-        return obj.author == request.user
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -24,7 +18,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs['post_id'])
